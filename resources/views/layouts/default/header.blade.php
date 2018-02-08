22fa974a364e0e5048c3d9cb4cd3415c6d0db4bf
Lucas Braga <lucas.braga@amlabs.com.br>
2018-02-08 16:36:28 -0200

    Materialize css

---
+++ resources/views/layouts/default/header.blade.php
@@ -0,0 +1,13 @@
+<div class="parallax-container">
+    <nav>
+        <div class="nav-wrapper">
+            <div class="container">
+                <a href="" class="brand-logo">{{ __('My Heroes - Forum') }}</a>
+            </div>
+        </div>
+    </nav>
+
+    <div class="parallax">
+        <img src="/img/help.jpg" alt="">
+    </div>
+</div>
